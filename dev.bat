594ce07e2b9918b3eedf525e8b4dc4e8f095d9ee
SaltIncluded <casperfj2011@gmail.com>
2023-03-06 12:47:03 +0100

    docker compose

---
+++ dev.bat
@@ -0,0 +1,4 @@
+docker-compose ^
+        -f services/hello1/docker-compose.yml ^
+        -f services/hello4/docker-compose.yml ^
+        up -d --build
---
+++ services/api/main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	// Create gin router
+	router := gin.Default()
+
+	// Create a default router group
+	root := router.Group("/")
+
+
+}
